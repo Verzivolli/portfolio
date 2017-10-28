15103d6b60e6cdb05635e735a20d449ddb4f9e2d
Verzivolli Ani <verzivolli.ani+c9@gmail.com>
2017-10-28 07:52:33 +0000

    impoved effect on mousevents on svg

--- javascript/svganimation.js
+++ javascript/svganimation.js
@@ -14,10 +14,10 @@ $("svg").mouseenter(function(){
   var svgID = $(this).attr("id");
   var circleID = "#" + svgID + "-circle";
 $(circleID).animate({"stroke-dashoffset": "0"},1000,"easeOutBounce",function(){
-    $(this).css({"stroke-dasharray": "63","stroke-dashoffset": "63"})
+    $(this).css({"stroke-dasharray": "0","stroke-dashoffset": "0"})
   });
 });
-$("svg").mouseout(function(){
+$("svg").mouseleave(function(){
   var svgID = $(this).attr("id");
   var strokeID = "#" + svgID + "-nofill";
   var centerID = "." + svgID + "-center";
@@ -25,6 +25,9 @@ $("svg").mouseout(function(){
   
   $(circleID).css({"stroke-dasharray": "63","stroke-dashoffset": "63"});
   $(strokeID).css({'fill': 'none', "stroke": "#e0e0e0"});
-  $(centerID).css({"fill": "#e0e0e0"})
+  $(centerID).css({"fill": "#e0e0e0"});
+  $(circleID).stop();
+  
+  //$(this).stop().animate({ left: -270 }, 500);
 });
 });
